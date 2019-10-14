83b9915a582b042cc4d23b1b9b309673b4a3c274
Gourabperu <gourabperu@outlook.com>
2019-10-14 15:21:13 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+
+  Culqi.publicKey = 'pk_test_8ALRMGmZESV7yN02';
+
+
+  Culqi.settings({
+    title: 'Culqi Store',
+    currency: 'PEN',
+    description: 'Polo/remera Culqi lover',
+    amount: 300
+  });
+
+
+
+$('#buyButton').on('click', function(e) {
+    // Abre el formulario con la configuración en Culqi.settings
+    Culqi.open();
+    e.preventDefault();
+});
+
+
+
+
+function culqi() {
+    if (Culqi.token) { // ¡Objeto Token creado exitosamente!
+        var token = Culqi.token.id;
+        alert('Se ha creado un token:' + token);
+        //En esta linea de codigo debemos enviar el "Culqi.token.id"
+        //hacia tu servidor con Ajax
+    } else { // ¡Hubo algún problema!
+        // Mostramos JSON de objeto error en consola
+        console.log(Culqi.error);
+        alert(Culqi.error.user_message);
+    }
+  };
+  
+  
